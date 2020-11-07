a677adcea0072c03178274de5c6cd1a278ed647c
yosaka <36161943+yosaka1138@users.noreply.github.com>
2020-11-08 05:58:29 +0900

    add library

---
+++ lib/inv.py
@@ -0,0 +1,15 @@
+# 逆元のテーブルを作成
+def cmb(n, k, mod, fac, ifac):    
+    k = min(k, n-k)
+    return fac[n] * ifac[k] * ifac[n-k] % mod
+
+def make_tables(mod, n):
+    fac = [1, 1] # 階乗テーブル・・・(1)
+    ifac = [1, 1] #逆元の階乗テーブル・・・(2)
+    inverse = [0, 1] #逆元テーブル・・・(3)
+ 
+    for i in range(2, n+1):
+        fac.append((fac[-1] * i) % mod)
+        inverse.append((-inverse[mod % i] * (mod//i)) % mod)
+        ifac.append((ifac[-1] * inverse[-1]) % mod)
+    return fac, ifac
