4678abbdcdbb2ae3140bc91c7ec4d63a47dfa292
yosaka1138 <fallingfluit@gmail.com>
2021-04-04 02:09:45 +0900

    add libraly

--- lib/dijkstra.py
+++ lib/dijkstra.py
@@ -1,22 +1,47 @@
-import heapq
-def dijkstra(s, N):
-    # 始点から各頂点への最短距離
-    d = [float('inf')]*N
-    d[s] = 0
-    # 各頂点が訪問済みかどうか
-    used = [False]*N
-    used[s] = True
-    # 仮の距離を記録するヒープ
-    que = []
-    for e in g[s]:
-        heapq.heappush(que, e)
-    while que:
-        u, v = heapq.heappop(que)
-        if used[v]:
-            continue
-        d[v] = u
-        used[v] = True
-        for e in g[v]:
-            if not used[e[1]]:
-                heapq.heappush(que, [e[0] + d[v], e[1]])
-    return d
+from heapq import heappop, heappush
+def dijkstra(s, N, adj):
+    dist = [float("inf")] * N
+    visited = [False] * N
+    dist[s] = 0
+    queue = [(0, s)]
+    while queue:
+        _, v = heappop(queue)
+        visited[v] = True
+        for to, cost in adj[v]:
+            if cost + dist[v] < dist[to] and not visited[to]:
+                dist[to] = dist[v] + cost
+                heappush(queue, (dist[to], to))
+    return dist
+
+"""AOJ sample
+from heapq import heappop, heappush
+V, E, r = map(int, input().split())
+adj = [[] for _ in range(V)]
+# print(adj)
+for i in range(E):
+    s, t, d = map(int, input().split())
+    # print(s, t, d)
+    adj[s].append((t, d))
+
+def dijkstra(s, N, adj):
+    dist = [float("inf")] * N
+    visited = [False] * N
+    dist[s] = 0
+    queue = [(0, s)]
+    while queue:
+        _, v = heappop(queue)
+        visited[v] = True
+        for to, cost in adj[v]:
+            if cost + dist[v] < dist[to] and not visited[to]:
+                dist[to] = dist[v] + cost
+                heappush(queue, (dist[to], to))
+    return dist
+
+dist = dijkstra(r, V, adj)
+for i in range(V):
+    if dist[i] == float("inf"):
+        print("INF")
+    else:
+        print(dist[i])
+
+"""
--- lib/union_find.py
+++ lib/union_find.py
@@ -1,4 +1,4 @@
-#coding: utf-8
+from collections import defaultdict
 
 class UnionFind():
     def __init__(self, n):
@@ -6,21 +6,19 @@ class UnionFind():
         self.parents = [-1] * n
 
     def find(self, x):
-        # xの根を探索
         if self.parents[x] < 0:
             return x
         else:
-            # 再帰的に根を探索
             self.parents[x] = self.find(self.parents[x])
             return self.parents[x]
 
     def union(self, x, y):
-        # 2つの木をマージする。
         x = self.find(x)
         y = self.find(y)
+
         if x == y:
             return
-        
+
         if self.parents[x] > self.parents[y]:
             x, y = y, x
 
@@ -30,24 +28,24 @@ class UnionFind():
     def size(self, x):
         return -self.parents[self.find(x)]
 
-    def are_same(self, x, y):
-        # 2つの要素の根が同じかどうかを調べる
+    def same(self, x, y):
         return self.find(x) == self.find(y)
 
     def members(self, x):
-        # xと同じ根の要素をリストで出力
         root = self.find(x)
-        return [i for i in range(self.n) if self.find(i) == self.n]
+        return [i for i in range(self.n) if self.find(i) == root]
 
     def roots(self):
-        # 根である要素をリストで出力
         return [i for i, x in enumerate(self.parents) if x < 0]
 
     def group_count(self):
         return len(self.roots())
 
     def all_group_members(self):
-        return {r: self.members(r) for r in self.roots()}
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
 
     def __str__(self):
-        return "\n".join("{} {}".format(r, self.members(r)) for r in self.roots())
+        return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
